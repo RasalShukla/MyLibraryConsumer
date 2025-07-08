9bbd215d8b02b02ac24046b237da1220959e5bb4
Rasal Shukla <Rasalshukla@outlook.com>
2025-07-08 20:01:55 +0530

    Initial Commit

---
+++ MyConsumer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MyConsumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This is a simple console application that uses the MyLibrary library.
+            // It prints a message to the console.
+            Console.WriteLine("Hello from MyConsumer!");
+            
+            // You can add more functionality here as needed.
+        }
+    }
+}
---
+++ MyLibrary/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary
+{
+    public class Calculator
+    {
+        public decimal Add(int a, int b)
+        {
+            return a + b;
+        }
+        public decimal Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public decimal Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public decimal Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
